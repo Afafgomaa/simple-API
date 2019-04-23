606b3275891a066cace03a3da49134b1e37882da
afaf gomaa <afafziden@gmail.com>
2019-04-24 01:41:30 +0200

    seedeing for product and reviews

--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -3,10 +3,16 @@
 /* @var $factory \Illuminate\Database\Eloquent\Factory */
 
 use App\review;
+use App\product;
 use Faker\Generator as Faker;
 
 $factory->define(review::class, function (Faker $faker) {
     return [
-        //
+        'product_id' => function(){
+          return product::all()->random();
+        },
+        'review' => $faker->paragraph,
+        'customer' => $faker->name,
+        'star' => $faker->numberBetween(0,5)
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        factory('App\product',50)->create();
+        factory('App\review',300)->create();
     }
 }
